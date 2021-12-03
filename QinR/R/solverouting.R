da9380ca1ab0e21ff887f9123c01de29e31cdf19
Angyalka Valcsics <36708283+angyalkavalcsics@users.noreply.github.com>
2021-12-03 18:14:42 -0500

    Update solverouting.R

--- QinR/R/solverouting.R
+++ QinR/R/solverouting.R
@@ -14,7 +14,7 @@
 #'           [,1]      [,2]      [,3]
 #' [1,] 0.4444444 0.3333333 0.2222222
 #'
-solve.routing <- function(R){
+solverouting <- function(R){
   n = nrow(R)
   m = ncol(R)
   I = diag(x = 1, nrow = n, ncol = m)
